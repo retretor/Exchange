3ac6efca02eb2e2bf647559700ae24145d6df36f
retretor <cherednichenko30igor@gmail.com>
2022-06-29 00:03:15 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/exchange/MainActivity.kt
+++ app/src/main/java/com/example/exchange/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.exchange
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -16,6 +17,8 @@ import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
+    private val url: String = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json"
+
     private var currency_1: TextView? = null
     private var currency_2: TextView? = null
     private var convert_btn: Button? = null
@@ -24,8 +27,16 @@ class MainActivity : AppCompatActivity() {
 
     private var cost: EditText? = null
 
+    private var currency_name: String? = null
+    private var currency_value: Double? = null
+
+    private var currencys_names = mutableListOf<String>()
+    private var currencys_values = mutableListOf<Double>()
+    private var currencys_cc = mutableListOf<String>()
 
 
+
+    @DelicateCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -37,47 +48,19 @@ class MainActivity : AppCompatActivity() {
         currency_2_value = findViewById(R.id.currency_2_value)
         cost = findViewById(R.id.cost)
 
+        CoroutineScope(Dispatchers.IO).launch {
+            async { fillArrays() }.await()
+        }
+
+
         convert_btn?.setOnClickListener {
             if (currency_1_value?.text?.toString()?.trim()?.equals("")!!)
             {
                 Toast.makeText(this, "Please enter a value", Toast.LENGTH_SHORT).show()
             }
-            else if(cost?.text?.toString()?.trim()?.equals("")!!)
-            {
-                Toast.makeText(this, "Please enter a cost", Toast.LENGTH_SHORT).show()
-            }
             else
             {
-                val currency_сс: String = currency_2?.text?.toString()?.trim()!!
-
-
-                /*GlobalScope.launch {
-                    var currency_name: String = withContext(Dispatchers.IO) { getCurrencyName(currency_сс) }
-                    var currency_value: String = withContext(Dispatchers.IO) { getCurrencyValue(currency_сс) }
-
-
-                    val currency_1_value_float = currency_1_value?.text?.toString()?.toFloat()!!
-                    val currency_value_float = currency_value.toFloat()
-
-                    val currency_2_value_float = currency_1_value_float * currency_value_float
-
-                    runOnUiThread {
-                        currency_2_value?.setText(currency_2_value_float.toString())
-                    }
-                }*/
-
-                val currency_name: String = getCurrencyName(currency_сс)
-                val currency_value: String = getCurrencyValue(currency_сс)
-
-
-                val currency_1_value_float = currency_1_value?.text?.toString()?.toFloat()!!
-                val currency_value_float = currency_value.toFloat()
-
-                val currency_2_value_float = currency_1_value_float * currency_value_float
-
-                currency_2_value?.setText(currency_2_value_float.toString())
-
-
+                convert()
             }
 
         }
@@ -85,35 +68,90 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun getCurrencyName(currency_сс: String): String {
+    fun convert() {
+        val currency_сс: String = currency_2?.text?.toString()?.trim()!!
 
-        val url: String = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json"
-        var currency_name: String = ""
 
+        /*GlobalScope.launch {
+            var currency_name: String = withContext(Dispatchers.IO) { getCurrencyName(currency_сс) }
+            var currency_value: String = withContext(Dispatchers.IO) { getCurrencyValue(currency_сс) }
+
+
+            val currency_1_value_float = currency_1_value?.text?.toString()?.toFloat()!!
+            val currency_value_float = currency_value.toFloat()
+
+            val currency_2_value_float = currency_1_value_float * currency_value_float
+
+            runOnUiThread {
+                currency_2_value?.setText(currency_2_value_float.toString())
+            }
+        }*/
+
+
+        /*GlobalScope.launch {
+            val job1 = launch { currency_name = getCurrencyName(currency_сс) }
+            val job2 = launch { currency_value = getCurrencyValue(currency_сс) }
+            job1.join()
+            job2.join()
+
+
+            Log.d("currency_name", currency_name!!)
+            Log.d("currency_value", currency_value!!)
+
+
+
+            currency_2?.setText(currency_name)!!
+
+            val currency_1_value_double = currency_1_value?.text?.toString()?.toDouble()!!
+            val currency_value_double = currency_value?.toDouble()!!
+
+            val currency_2_value_float = currency_1_value_double * currency_value_double
+
+            currency_2_value?.setText(currency_2_value_float.toString())
+        }*/
+        currency_name = getCurrencyName(currency_сс)
+        currency_value = getCurrencyValue(currency_сс)
+
+
+        Log.d("currency_name", currency_name!!)
+        Log.d("currency_value", currency_value.toString())
+
+
+        currency_2?.setText(currency_name)!!
+
+        val currency_1_value_double = currency_1_value?.text?.toString()?.toDouble()!!
+
+        val currency_2_value_float = currency_1_value_double * currency_value!!
+
+        currency_2_value?.setText(currency_2_value_float.toString())
+    }
+
+    suspend fun fillArrays() {
         val response = URL(url).readText()
-
         val jsonArray = JSONArray(response)
         for (i in 0 until jsonArray.length()) {
             val jsonObject = jsonArray.getJSONObject(i)
-            if (jsonObject.getString("cc").equals(currency_сс)) {
-                currency_name = jsonObject.getString("txt")
+            currencys_names.add(jsonObject.getString("txt"))
+            currencys_values.add(jsonObject.getDouble("rate"))
+            currencys_cc.add(jsonObject.getString("cc"))
+        }
+    }
+
+    fun getCurrencyName(currency_сс: String): String {
+        var currency_name: String = ""
+        for (i in 0 until currencys_names.size) {
+            if (currencys_cc[i].equals(currency_сс)) {
+                currency_name = currencys_names[i]
             }
         }
         return currency_name
     }
 
-    fun getCurrencyValue(currency_сс: String): String {
-
-        val url: String = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json"
-        var currency_value: String = ""
-
-        val response = URL(url).readText()
-
-        val jsonArray = JSONArray(response)
-        for (i in 0 until jsonArray.length()) {
-            val jsonObject = jsonArray.getJSONObject(i)
-            if (jsonObject.getString("cc").equals(currency_сс)) {
-                currency_value = jsonObject.getString("rate")
+    fun getCurrencyValue(currency_сс: String): Double {
+        var currency_value: Double = 0.0
+        for (i in 0 until currencys_values.size) {
+            if (currencys_cc[i].equals(currency_сс)) {
+                currency_value = currencys_values[i]
             }
         }
         return currency_value
